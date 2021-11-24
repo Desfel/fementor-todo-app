3fc49dcae67d507982735703052db6b4d91ccec3
Desfel <wil.southiphonh@gmail.com>
2021-11-24 01:54:55 -0500

    :sparkles: finalize

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,5 @@
 <template>
   <div id="app">
-    <nav-bar></nav-bar>
     <div class="main-wrapper">
       <router-view />
     </div>
@@ -20,13 +19,12 @@
   </div>
 </template>
 <script>
-import NavBar from '@/components/NavBar'
 import NewContentAvailableToastr from '@/components/NewContentAvailableToastr'
 import AppleAddToHomeScreenModal from '@/components/AppleAddToHomeScreenModal'
 import { mapState, mapActions, mapGetters } from 'vuex'
 
 export default {
-  components: { NavBar, NewContentAvailableToastr, AppleAddToHomeScreenModal },
+  components: { NewContentAvailableToastr, AppleAddToHomeScreenModal },
   computed: {
     ...mapGetters('app', ['newContentAvailable']),
     ...mapState('app', ['showAddToHomeScreenModalForApple', 'refreshingApp'])
@@ -39,8 +37,25 @@ export default {
 </script>
 
 <style lang="scss">
+@import '@/theme/variables.scss';
 body {
   margin: 0;
+
+  * {
+    box-sizing: border-box;
+  }
+
+  p,
+  h1,
+  a,
+  input {
+    font-family: $textFont;
+    margin: 0;
+  }
+
+  img {
+    vertical-align: middle;
+  }
 
   a {
     font-weight: 500;
@@ -61,29 +76,9 @@ body {
       right: 10px;
     }
 
-    .apple-add-to-home-screen-modal {
-      position: absolute;
-      bottom: 0;
-      right: 0;
-      top: 0;
-      left: 0;
-      height: fit-content;
-      width: fit-content;
-      margin: auto;
-      z-index: 1000;
-    }
-
     .main-wrapper {
-      margin-top: 3.6rem;
-      padding: 20px;
-
       .page-wrapper {
-        width: 60%;
         margin: auto;
-
-        @media screen and (max-width: 1000px) {
-          width: 100%;
-        }
       }
     }
   }
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,15 +1,78 @@
 <template>
-  <div class="page-wrapper">
-    <h1 class="home-page-title">{{ appTitle }}</h1>
-    <img alt="logo-bento" class="logo" src="@/assets/img/bento-starter.svg" />
+  <div class="page-wrapper" :class="{'theme-light' : isLight}">
+    <div class="app-wrapper">
+      <section class="header-section">
+        <h1>Todo</h1>
 
-    <a
-      rel="noopener"
-      class="documentation-link"
-      target="_blank"
-      href="https://bento-starter.netlify.com/"
-      >Documentation →</a
-    >
+        <a href="#" class="theme-toggler" @click="toggleTheme">
+          <img v-if="!isLight" src="@/assets/img/icon-sun.svg" alt="Light theme" />
+          <img v-if="isLight" src="@/assets/img/icon-moon.svg" alt="Dark theme" />
+        </a>
+      </section>
+
+      <section class="task-form-wrapper">
+        <form class="task-form" action="#" method="POST" @submit="addTask">
+          <div class="task-container">
+            <div class="task-cell">
+              <span class="checkbox" @click="addTask">
+                <span class="checkbox-inner"></span>
+                <img class="checkmark" src="@/assets/img/icon-check.svg">
+              </span>
+
+              <input
+                v-model="taskValue"
+                type="text"
+                value=""
+                name="task"
+                placeholder="Create a new todo..."
+              />
+            </div>
+          </div>
+        </form>
+      </section>
+
+      <section class="task-container task-list-wrapper">
+        <div class="task-cell" v-for="task in tasksArray" v-bind:key="task.id"
+          :class="{
+            'is-completed': completedTasks.indexOf(task.id) !== -1,
+            'is-visible':
+              (completedTasks.indexOf(task.id) === -1 && activeFilter === 'active') ||
+              (completedTasks.indexOf(task.id) !== -1 && activeFilter === 'completed') ||
+              activeFilter === '*'
+        }">
+          <span class="checkbox" @click="toggleCompleteTask(task.id)">
+            <span class="checkbox-inner"></span>
+            <img class="checkmark" src="@/assets/img/icon-check.svg">
+          </span>
+
+          <p v-text="task.task"></p>
+
+          <span class="remove-task" @click="removeTask(task.id)">
+            <img src="@/assets/img/icon-cross.svg" />
+          </span>
+        </div>
+
+        <div class="task-actions-row">
+          <p class="item-counts" v-text="`${tasksArray.length} items left`"></p>
+
+          <div class="task-filters desktop-filters">
+            <a href="#" :class="{'is-active' : activeFilter === '*'}" @click="selectFilter('*')">All</a>
+            <a href="#" :class="{'is-active' : activeFilter === 'active'}" @click="selectFilter('active')">Active</a>
+            <a href="#" :class="{'is-active' : activeFilter === 'completed'}" @click="selectFilter('completed')">Completed</a>
+          </div>
+
+          <a href="#" class="clear-completed" @click="clearCompletedTasks">Clear completed</a>
+        </div>
+      </section>
+
+      <div class="task-actions-row mobile-filters task-container">
+        <div class="task-filters">
+          <a href="#" :class="{'is-active' : activeFilter === '*'}" @click="selectFilter('*')">All</a>
+          <a href="#" :class="{'is-active' : activeFilter === 'active'}" @click="selectFilter('active')">Active</a>
+          <a href="#" :class="{'is-active' : activeFilter === 'completed'}" @click="selectFilter('completed')">Completed</a>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -17,6 +80,15 @@
 import { mapState } from 'vuex'
 
 export default {
+  data() {
+    return {
+      isLight: false,
+      taskValue: '',
+      tasksArray: [],
+      completedTasks: [],
+      activeFilter: '*'
+    }
+  },
   head() {
     return {
       title: {
@@ -31,39 +103,55 @@ export default {
       ]
     }
   },
+  methods: {
+    toggleTheme(e) {
+      e.preventDefault()
+      this.isLight = !this.isLight
+    },
+    addTask(e) {
+      e.preventDefault()
+
+      const trimmedValue = this.taskValue.trim()
+
+      if(trimmedValue !== '') {
+        this.tasksArray.push({id: this.tasksArray.length, task: this.taskValue})
+      }
+    },
+    toggleCompleteTask(taskId) {
+      if(this.completedTasks.indexOf(taskId) === -1) {
+        this.completedTasks.push(taskId)
+      } else {
+        this.completedTasks.splice(this.completedTasks.indexOf(taskId), 1)
+      }
+    },
+    removeTask(taskId) {
+      this.tasksArray.forEach((task, index) => {
+        if(task.id === taskId) {
+          this.tasksArray.splice(index, 1)
+        }
+      })
+      this.completedTasks.splice(this.completedTasks.indexOf(taskId), 1)
+    },
+    clearCompletedTasks() {
+      this.completedTasks.forEach(completedTask => {
+        this.tasksArray.forEach((task, index) => {
+          if(task.id === completedTask) {
+            this.tasksArray.splice(index, 1)
+          }
+        })
+      })
+      this.completedTasks = []
+    },
+    selectFilter(filter) {
+      this.activeFilter = filter
+      console.log(this.activeFilter)
+    }
+  },
   computed: mapState('app', ['appTitle'])
 }
 </script>
 
 <style lang="scss" scoped>
 @import '@/theme/variables.scss';
-
-.page-wrapper {
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
-
-  .logo {
-    margin-bottom: 3rem;
-  }
-
-  .home-page-title {
-    text-align: center;
-  }
-
-  .documentation-link {
-    display: inline-block;
-    font-size: 1.2rem;
-    color: #fff;
-    background-color: #5d6788;
-    padding: 0.8rem 1.6rem;
-    border-radius: 4px;
-    transition: background-color 0.1s ease;
-    box-sizing: border-box;
-    text-decoration: none;
-    width: fit-content;
-    font-weight: 500;
-  }
-}
+@import '@/theme/main.scss';
 </style>
